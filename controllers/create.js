4ea934be5463760da9d2d605aca56e8fb88a80cf
SuhasS2 <73155132+SuhasS2@users.noreply.github.com>
2020-12-02 13:40:47 +0530

    Update create.js
    Changes Made on create file

--- controllers/create.js
+++ controllers/create.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const testData = require('../models/studentSchema');
+const testData = require('../models/registratioAndTestSchema');
 
 module.exports = function () {
+    router.post('/create-data', async (req, res) => {
     const { grade, addRegStartDate, addRegStopDate, addTestStartDate, addTestStopDate } = req.body;
     await testData.insertOne({
         grade: grade, registrationStartDate: addRegStartDate, registrationStoptDate: addRegStopDate,
         testStartDate: addTestStartDate, testStopDate: addTestStopDate
     })
-
+        res.send({ message: `Test Data Added successfully` });
+    });
     return router;
-}
+};
